176ad7eadd1affe769e8fb9a1e45c1a506253061
feliciafrncs <Felicia.Immanuel@campus.lmu.de>
2025-12-06 15:38:01 +0100

    bone quality

--- Analysis/pairsdata.R
+++ Analysis/pairsdata.R
@@ -225,3 +225,20 @@ pairs_ind <- pairs_all |> filter(tomb1 != "Elateia T46 56 62", tomb2 != "Elateia
 
 pairs_group <- pairs_all |> filter(!tomb1 %in% c("Elateia T46", "Elateia T56", "Elateia T62"),
                                    !tomb2 %in% c("Elateia T46", "Elateia T56", "Elateia T62"))
+
+pairs_ind <- pairs_ind %>%
+  mutate(
+    bone_quality = case_when(
+      grepl("petrous", skeletal_element1) & grepl("petrous", skeletal_element2) ~ "high-high",
+      grepl("petrous", skeletal_element1) | grepl("petrous", skeletal_element2) ~ "high-low",
+      TRUE ~ "low-low"
+    )
+  )
+pairs_group <- pairs_group |>
+  mutate(
+    bone_quality = case_when(
+      grepl("petrous", skeletal_element1) & grepl("petrous", skeletal_element2) ~ "high-high",
+      grepl("petrous", skeletal_element1) | grepl("petrous", skeletal_element2) ~ "high-low",
+      TRUE ~ "low-low"
+    )
+  )
