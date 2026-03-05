0d4950e22cf2bd9244bec3545d7edf6195d909e3
Anh Dang <anhdang@Mac.fritz.box>
2026-03-06 00:36:15 +0100

    editing + models

--- Models/fit_models.R
+++ Models/fit_models.R
@@ -1,5 +1,5 @@
 # This script loads the prepared pair datasets and fits the statistical models
-# used in the analysis. It runs Bayesian mixed models for binary relatedness
+# used in the analysis. It runs Bayesian mixed models 
 # and a Gaussian regression model for SNP overlap, and saves all fitted models
 # as .rds files in the folder Models/Saved models.
 
@@ -116,7 +116,7 @@ saveRDS(
   file = "Models/Saved models/imp_binary_group_unrel3rd.rds"
 )
 
-# 7. Robust check model (separated): set prior N(0,1)
+# 7. Robust check model (separated): set prior N(0,1) ----
 weak_prior <- set_prior("normal(0, 1)", class = "b")
 
 imp_binary_separate_nobone_prior <- update(imp_binary_separate_nobone,
@@ -132,7 +132,7 @@ saveRDS(
   file = "Models/Saved models/imp_binary_separate_nobone_prior.rds"
 )
 
-# 8. Robust check model (grouped): set prior N(0,1)
+# 8. Robust check model (grouped): set prior N(0,1) ----
 imp_binary_group_nobone_prior <- update(imp_binary_group_nobone,
                                         prior = weak_prior,
                                         seed = 123)
@@ -146,7 +146,148 @@ saveRDS(
   file = "Models/Saved models/imp_binary_group_nobone_prior.rds"
 )
 
-# 9. Model Overlap GLM
+# 9. Robust check model (separated): within tomb pairs only ---- 
+imp_binary_separate_withintomb <- update(imp_binary_separate_nobone, 
+                                         formula. = related_binary ~ sex_pair + age_pair + 
+                                           (1 | mm(individual1_id, individual2_id)) + (1 | tomb1),
+                                         newdata = pairs_ind_withintomb,
+                                         seed = 123)
+
+# Inspect model
+summary(imp_binary_separate_withintomb)
+
+# Save fitted model
+saveRDS(
+  imp_binary_separate_withintomb,
+  file = "Models/Saved models/imp_binary_separate_withintomb.rds"
+)
+
+# 10. Robust check model (grouped): within tomb pairs only ----
+imp_binary_group_withintomb <- update(imp_binary_separate_withintomb, newdata = pairs_group_withintomb,
+                                      seed = 123)
+
+# Inspect model
+summary(imp_binary_group_withintomb)
+
+# Save fitted model
+saveRDS(
+  imp_binary_group_withintomb,
+  file = "Models/Saved models/imp_binary_separate_withintomb.rds"
+)
+
+# 11. Robust check model (separated): within tomb pairs only and 3rd degree as unrelated ----
+pairs_ind_withintomb_unrel3rd <- pairs_ind_unrel3rd[pairs_ind_unrel3rd$same_tomb == TRUE, ]
+pairs_group_withintomb_unrel3rd <- pairs_group_unrel3rd[pairs_group_unrel3rd$same_tomb == TRUE, ]
+
+imp_binary_separate_withintomb_unrel3rd <- update(imp_binary_separate_withintomb,
+                                                  newdata = pairs_ind_withintomb_unrel3rd,
+                                                  seed = 123)
+# Inspect model
+summary(imp_binary_separate_withintomb_unrel3rd)
+
+# Save fitted model
+saveRDS(
+  imp_binary_separate_withintomb_unrel3rd,
+  file = "Models/Saved models/imp_binary_separate_withintomb_unrel3rd.rds"
+)
+
+# 12. Robust check model (grouped): within tomb pairs only and 3rd degree as unrelated ----
+imp_binary_group_withintomb_unrel3rd <- update(imp_binary_group_withintomb,
+                                               newdata = pairs_group_withintomb_unrel3rd,
+                                               seed = 123)
+# Inspect model
+summary(imp_binary_group_withintomb_unrel3rd)
+
+# Save fitted model
+saveRDS(
+  imp_binary_group_withintomb_unrel3rd,
+  file = "Models/Saved models/imp_binary_group_withintomb_unrel3rd.rds"
+)
+
+# 13. Robust check model (separated): standard threshold with ske.elem ----
+all_binary_separate_cat <- brm(related_binary ~ sex_pair + age_pair + bone_quality + same_tomb +
+                                 (1 | mm(individual1_id, individual2_id)) +
+                                 (1 | mm(tomb1, tomb2)),
+                               data = pairs_ind_all,
+                               family = bernoulli(),
+                               control = list(
+                                 adapt_delta = 0.999,   # The "Baby Steps" (Default 0.8)
+                                 max_treedepth = 15     # Allow longer trajectories (Default 10)
+                               ),
+                               
+                               # INCREASE ITERATIONS
+                               # 6000 total = 3000 warmup (thrown away) + 3000 sampling (kept)
+                               iter = 6000, 
+                               warmup = 3000, 
+                               
+                               chains = 4, 
+                               cores = 4, 
+                               seed = 123)
+# Inspect model
+summary(all_binary_separate_cat)
+
+# Save fitted model
+saveRDS(
+  all_binary_separate_cat,
+  file = "Models/Saved models/all_binary_separate_cat.rds"
+)
+
+# 14. Robust check model (grouped): standard threshold with ske.elem ----
+all_binary_group_cat <- brm(related_binary ~ sex_pair + age_pair + bone_quality + same_tomb +
+                              (1 | mm(individual1_id, individual2_id)) +
+                              (1 | mm(tomb1, tomb2)),
+                            data = pairs_group_all,
+                            family = bernoulli(),
+                            control = list(
+                              adapt_delta = 0.999,   # The "Baby Steps" (Default 0.8)
+                              max_treedepth = 15     # Allow longer trajectories (Default 10)
+                            ),
+                            
+                            # INCREASE ITERATIONS
+                            # 6000 total = 3000 warmup (thrown away) + 3000 sampling (kept)
+                            iter = 6000, 
+                            warmup = 3000, 
+                            
+                            chains = 4, 
+                            cores = 4, 
+                            seed = 123)
+# Inspect model
+summary(all_binary_group_cat)
+
+# Save fitted model
+saveRDS(
+  all_binary_group_cat,
+  file = "Models/Saved models/all_binary_group_cat.rds"
+)
+
+
+# 15. Robust check model (separated): standard threshold with main formula ----
+all_binary_separate_nobone <- update(imp_binary_separate_nobone,
+                                     newdata = pairs_ind_all,
+                                     seed = 123)
+# Inspect model
+summary(all_binary_separate_nobone)
+
+# Save fitted model
+saveRDS(
+  all_binary_separate_nobone,
+  file = "Models/Saved models/all_binary_separate_nobone.rds"
+)
+
+# 16. Robust check model (grouped): standard threshold with main formula ----
+all_binary_group_nobone <- update(imp_binary_group_nobone,
+                                  newdata = pairs_group_all,
+                                  seed = 123)
+# Inspect model
+summary(all_binary_group_nobone)
+
+# Save fitted model
+saveRDS(
+  all_binary_group_nobone,
+  file = "Models/Saved models/all_binary_group_nobone.rds"
+)
+
+# . Model Overlap GLM
 # Gaussian GLM with identity link
 m_overlap_glm <- glm(
   overlap_sc ~ same_tomb + sex_pair + age_pair +
@@ -165,7 +306,7 @@ saveRDS(
   file = "Models/Saved models/m_overlap_glm.rds"
 )
 
-# 10. Relatedness Bayesian Regression Modeling (BRM)
+# . Relatedness Bayesian Regression Modeling (BRM)
 priors <- c(
   prior(normal(0, 1), class = "b"),
   prior(normal(0, 1), class = "Intercept"),
